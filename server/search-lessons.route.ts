a3d5629e69b940b26b87949acbf1adac9089c3e7
Lazarchuk <a.lazarchuk@cartsys.com.ua>
2019-10-24 11:20:55 +0300

    #release

--- server/search-lessons.route.ts
+++ server/search-lessons.route.ts
@@ -4,31 +4,40 @@ import { setTimeout } from 'timers';
 
 export function searchLessons(req: Request, res: Response) {
     const queryParams = req.query;
+    console.log( queryParams ) // console.log( JSON.stringify(queryParams) )
+    /* receive request query params */
     const courseId = queryParams.courseId,
           filter = queryParams.filter || '',
           sortOrder = queryParams.sortOrder,
           pageNumber = parseInt(queryParams.pageNumber) || 0,
           pageSize = parseInt(queryParams.pageSize);
 
-    console.log( queryParams )
-    // console.log( JSON.stringify(queryParams) )
-
+    /* SELECT * FROM db WHERE courseId = <courseId> ORDER BY id */
     let lessons = Object.values(LESSONS)
       .filter(lesson => lesson.courseId == courseId)
       .sort((l1, l2) => l1.id - l2.id);
+    // console.log( lessons )
+
+    /* do filter to records receive db */
     if (filter) {
        lessons = lessons.filter(
-         lesson => lesson.description.trim()
+         l => l.description.trim()
            .toLowerCase()
            .search(filter.toLowerCase()) >= 0);
     }
+    // console.log( lessons )
+
+    /* do sort order to records receive db */
     if (sortOrder == "desc") {
         lessons = lessons.reverse();
     }
 
+    /* select page by position to records receive db */
     const initialPos = pageNumber * pageSize;
     const lessonsPage = lessons.slice(initialPos, initialPos + pageSize);
+    console.log( lessonsPage )
     setTimeout(() => {
-        res.status(200).json({ payload: lessonsPage });
+        res.status(200)
+            .json({ payload: lessonsPage });
     },1000);
 }
--- server/server.ts
+++ server/server.ts
@@ -1,18 +1,11 @@
 import * as express from 'express';
-import { Application} from 'express';
-import { getAllCourses, getCourseById } from './get-courses.route';
+import { Application } from 'express';
 import { searchLessons } from './search-lessons.route';
 
 const app: Application = express();
 
-app.route('/api/courses').get(getAllCourses);
-app.route('/api/courses/:id').get(getCourseById);
-app.route('/api/lessons').get(searchLessons);
+app.route('/api/lessons').get(searchLessons); //TODO:  http://localhost:4200/api/lessons?courseId=1&filter=&sortOrder=asc&pageNumber=1&pageSize=3
 
 const httpServer = app.listen(9000, () => {
     console.log('HTTP REST API Server running at http://localhost:' + httpServer.address().port);
 });
-
-
-
-
