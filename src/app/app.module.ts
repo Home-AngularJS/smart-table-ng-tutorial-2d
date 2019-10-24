f4a7dadb88b1954589c3f4acacbedd46b7d16b4d
Lazarchuk <a.lazarchuk@cartsys.com.ua>
2019-10-24 21:53:03 +0300

    #dev  app: modify users service (remote) select data from db

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { UsersModule } from './layout/users/users.module';
 import { SmartTableModule } from 'smart-table-ng';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { UsersComponent } from "./layout/users/users.component";
+import { UsersRest } from "./core/service/users.rest";
 
 @NgModule({
   imports: [
       BrowserModule,
       FormsModule,
       SmartTableModule,
-      UsersModule
+      CommonModule,
+      HttpClientModule
   ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, UsersComponent ],
+  providers: [
+      UsersRest
+  ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
